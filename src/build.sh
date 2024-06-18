c64c79b08222151dd7b0edfffe10b94f617bf091
Himel Das <60639301+thehimel@users.noreply.github.com>
2024-06-19 00:40:56 +0200

    updated build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
 cd client && npm install --force && npx vite build && rm -rf node_modules && cd ..
 python3.9 -m pip install --upgrade pip
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --no-input
 python3.9 manage.py migrate
